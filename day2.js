837763dacecc0523921d99dd58679a3009c28bd2
Priyanshi Sharma <priyanshisharma085@gmail.com>
2022-08-02 05:20:16 +0530

    first commit

---
+++ day2.js
@@ -0,0 +1,70 @@
+console.log("day2");
+
+// Q1. What are different datatypes in Javascript?
+
+// A). Primitive Datatype B). Non-Primitive Datatype
+
+// A). Primitive Datatypes-
+
+// 1. Number
+// 2. Null
+// 3. Boolean
+// 4. Symbol
+// 5. String
+// 6. Big int
+// 7. Undefined
+
+// B). Non-Primitive Datatypes-
+
+// 1. Object
+// 2. Array
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+// Q2. In how many ways can we declare a variable?
+
+// 3ways- var,let,const
+
+// 1)Let is an  Block scope 
+// 2)Let Cant be Redeclared 
+// 3)Let can be Modified Later
+
+// 1)Const is an  Block scope 
+// 2)Const Cant be Redeclared 
+// 3)Const cant be Modified Later
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+// Q3. What is Hoisting?
+
+// Hoisting :Hoisting is a Javascript Mechanism where variables and function declarations are moved 
+// to top of there scope before the code execution 
+
+// Note:Only Declarations are hoisted not the initilizations 
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+// Q4. What is an temperal dead zone ?
+
+// A)It is the area of a block where a variable is inaccessible until the moment computer initilizes
+// it with a value 
+
+// TDZ occurs when we declare a variable of Let and Const keyword.
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+// Q5. What is the Difference between == & === Operator  ?
+
+// == only checks the value 
+// === checks the both value and the data type 
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+// Q6. What are the Different types of Operators in Javascript
+
+// 1)Arithmetic Operators
+// 2)Comparision Operators 
+// 3)Logical Operators
+// 4)Relational Operators
+// 5)Conditional Operator
+// 6)Bitwise Operator 
